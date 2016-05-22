bdb3e49afc50bf4aed284fffafc929ab30f688d2
Jay Barra <jay.f.barra@gmail.com>
2016-05-22 18:19:50 -0400

    updated travis ci build script trying to figure out why jest isn't working

--- src/server/server.js
+++ src/server/server.js
@@ -16,7 +16,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 
 app.use('/dist/', express.static(path.resolve(path.join(__dirname, '../../dist'))));
 app.use('/', routes);
